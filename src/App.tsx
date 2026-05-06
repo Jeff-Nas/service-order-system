8c2fd2dc860cb78c850f8e5fcf3e1ff4aaf19b86
Jeferson Nascimento <jeferson.ifce@gmail.com>
2026-05-06 10:29:45 -0300

    Configurando o tipo corretamente

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,10 @@
 import "./global.css";
-
-import { ClientField } from "./components/form/clientField";
+import OSForm from "./components/form/osForm";
 function App() {
   return (
     <div className="p-3">
       <h1 className="text-2xl">Início da Ordem de Serviço</h1>
-      <ClientField />
+      <OSForm />
     </div>
   );
 }
